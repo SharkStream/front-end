b00c628e3f4582b7790f935a7856069b9b9f5a80
SharkStream <395927418@qq.com>
2023-02-27 15:34:56 +0800

    One simple demo of CURD framework

--- React/fullStackDemo/Lesson 01/controllers/usersController.js
+++ React/fullStackDemo/Lesson 01/controllers/usersController.js
@@ -3,3 +3,42 @@ const Note = require('../models/Note')
 
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
+
+// @desc Get all users
+// @route Get /users
+// @access Private
+
+const getAllUsers = asyncHandler(async(req, res) => {
+
+})
+
+// @desc Create new user
+// @route POST /users
+// @access Private
+
+const createNewUser = asyncHandler(async(req, res) => {
+    
+})
+
+// @desc Update user
+// @route PATCH /users
+// @access Private
+
+const updateUser = asyncHandler(async(req, res) => {
+    
+})
+
+// @desc Delete user
+// @route DELETE /users
+// @access Private
+
+const deleteUser = asyncHandler(async(req, res) => {
+    
+})
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser
+}
