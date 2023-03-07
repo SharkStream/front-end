111ce282126ad11f8a14c7d2f7088b0eeba64711
SharkStream <395927418@qq.com>
2023-03-07 09:16:15 +0800

    use createAsyncThunk to implement function

--- Redux/src/features/posts/postsSlice.js
+++ Redux/src/features/posts/postsSlice.js
@@ -1,35 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { nanoid } from "@reduxjs/toolkit";
+import { nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from 'date-fns'
+import axios from 'axios'
 
-const initialState = [
-    {
-        id: '1',
-        title: "Haha",
-        content: "Hello,My first time to come here,I'm freshers",
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-    {
-        id: '2',
-        title: "Babab",
-        content: "No need to tell, you just take over me",
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const initialState = {
+    posts: [],
+    status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
+    error: null
+}
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    try {
+        const response = await axios.get(POSTS_URL)
+        return [...response.data]
+    } catch (err) {
+        return err.message;
     }
-]
+})
 
 export const postsSlice = createSlice({
     name: 'posts',
@@ -37,7 +27,7 @@ export const postsSlice = createSlice({
     reducers: {
         postAdded: {
             reducer(state, action) {
-                state.push(action.payload)
+                state.posts.push(action.payload)
             },
             prepare(title, content, userId) {
                 return {
@@ -60,15 +50,45 @@ export const postsSlice = createSlice({
         },
         reactionAdded: (state, actions) => {
             const { postId, reaction } = actions.payload
-            const existingPost = state.find(post => post.id === postId)
+            const existingPost = state.posts.find(post => post.id === postId)
             if (existingPost) {
                 existingPost.reactions[reaction]++
             }
         }
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchPosts.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchPosts.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                // Adding date and reactions
+                let min = 1;
+                const loadedPosts = action.payload.map(post => {
+                    post.date = sub(new Date(), { minutes: min++ }).toISOString()
+                    post.reactions = {
+                        thumbsUp: 0,
+                        wow: 0,
+                        heart: 0,
+                        rocket: 0,
+                        coffee: 0
+                    }
+                    return post;
+                })
+
+                state.posts = state.posts.concat(loadedPosts)
+            })
+            .addCase(fetchPosts.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.error.message
+            })
     }
 })
 
-export const selectAllPosts = state => state.posts
+export const selectAllPosts = state => state.posts.posts
+export const getPostsStatus = state => state.posts.status
+export const getPosesError = state => state.posts.error
 
 export const { postAdded, reactionAdded } = postsSlice.actions
 
--- Redux/src/index.js
+++ Redux/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
 import { store } from './app/store'
 import App from './App'
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers())
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
